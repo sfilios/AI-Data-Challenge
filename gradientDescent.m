ca752791157b2932b7775a0cafff9e1d172c79ec
Stephen <sfilios@users.noreply.github.com>
2016-08-03 18:02:34 -0700

    Update gradientDescent.m

--- gradientDescent.m
+++ gradientDescent.m
@@ -1,12 +1,16 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
-%GRADIENTDESCENT Performs gradient descent to learn theta
-%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
-%   taking num_iters gradient steps with learning rate alpha
+
+% Perform gradient descent on the cost of theta, to try to minimize this value.
+% Takes the given number of iterations, as well as the value given for alpha 
+% and uses these for the operation.
 
 
 m = length(y); % number of training examples
+
+% matrix holding the costs as gradient descent proceeds
 J_history = zeros(num_iters, 1);
 
+%performs the descent
 for iter = 1:num_iters
 	
 	d = pinv(m) * X'*(X*theta - y);
